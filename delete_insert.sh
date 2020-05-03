d7f4e764d86822044db1728551f1fe1519181a54
細田智大 <hossohdayo@hossonoMacBook.local>
2020-05-03 19:07:35 +0900

    first commit

---
+++ delete_insert.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+# 処理開始
+echo "[INFO] 処理開始"
+
+# 事前チェック
+if [ $# -ne 3 ]; then
+  echo "指定された引数は$#個です。" 1>&2
+  echo "実行するには3個の引数が必要です。" 1>&2
+  exit 1
+fi
+
+# 変数定義
+MYSQL_SCHEMA="mydb"
+ROOT_DIRECTORY="/usr/local/etc"
+CMD_MYSQL="mysql --defaults-extra-file=$ROOT_DIRECTORY/my.cnf -N --show-warnings $MYSQL_SCHEMA"
+TEAM_ID=$1
+NUMBER=$2
+URL=$3
+
+# SQLの指定
+# カラムにアスタリスク（*）を使うとうまくいかなかったので、一つずつ指定
+QUERY1="SELECT count(team_id) FROM player where team_id='"
+QUERY2="' and number='"
+QUERY3="'"
+QUERY=$QUERY1$TEAM_ID$QUERY2$NUMBER$QUERY3
+echo "[INFO] SQL:$QUERY"
+# シェルを実行、実行ログを受け取る
+VALUE=`echo ${QUERY} | ${CMD_MYSQL}`
+# 結果の終了コードチェック
+if [ $? -eq 0 ]; then
+    echo "[INFO] SELECT OK"
+else
+    echo "[ERROR] SQL異常終了"
+    exit 1
+fi
+if [ $VALUE != "0" ] || [ $VALUE != "1" ]; then
+    echo "[INFO] 選手存在確認${VALUE}件"
+else
+    echo "[ERROR] VALUEが0でなく1でもないため異常終了"
+    exit 1
+fi
+
+# スクレイピング開始
+echo "[INFO] スクレイピング開始"
+SCRPAING_RES=`python3 player_scraping.py $URL`
+
+# 結果のチェック
+if [ $? -eq 0 ]; then
+    echo "[INFO] 処理終了"
+else
+    echo "[ERROR] 予期せぬエラーが発生 異常終了"
+    exit 1
+fi
+
+# スクレイピング結果をインサート
+echo ${SCRPAING_RES} | ${CMD_MYSQL}
+
+# 結果の終了コードチェック
+if [ $? -eq 0 ]; then
+    echo "[INFO] insertOK"
+    exit 0
+else
+    echo "[ERROR] insertSQL異常終了"
+    exit 1
+fi
---
+++ player_scraping.py
@@ -0,0 +1,93 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime as dt
+import sys
+
+args = sys.argv
+url = "https://baseball.yahoo.co.jp/npb/player/" + args[1] + "/top"
+#r = requests.get('https://baseball.yahoo.co.jp/npb/player/1600127/top')
+r = requests.get(url)
+data = BeautifulSoup(r.text, 'html.parser')
+
+def get_team():
+    elems = data.find_all("h1", class_="bb-head01__title")
+
+    for e in elems:
+        if e.getText() == "埼玉西武ライオンズ":
+            team = 'L'
+        elif e.getText() == "福岡ソフトバンクホークス":
+            team = 'H'
+        elif e.getText() == "東北楽天ゴールデンイーグルス":
+            team = 'E'
+        elif e.getText() == "千葉ロッテマリーンズ":
+            team = 'M'
+        elif e.getText() == "北海道日本ハムファイターズ":
+            team = 'F'
+        elif e.getText() == "オリックス・バファローズ":
+            team = 'Bs'
+        elif e.getText() == "読売ジャイアンツ":
+            team = 'G'
+        elif e.getText() == "横浜ＤｅＮＡベイスターズ":
+            team = 'De'
+        elif e.getText() == "阪神タイガース":
+            team = 'T'
+        elif e.getText() == "広島東洋カープ":
+            team = 'C'
+        elif e.getText() == "中日ドラゴンズ":
+            team = 'D'
+        elif e.getText() == "東京ヤクルトスワローズ":
+            team = 'Ys'
+        else:
+            team = 'エラー'
+    return team
+
+def get_player_number():
+    elems3 = data.find_all("p", class_="bb-profile__number")
+    number = elems3[0].getText()
+    return number
+
+def get_player_position():
+    elems4 = data.find_all("p", class_="bb-profile__position")
+    position = elems4[0].getText()
+    return position
+
+def get_player_name():
+    elems5 = data.find_all("h1")
+    name = elems5[1].getText()
+    return name
+
+def get_player_data():
+    elems2 = data.find_all("dd", class_="bb-profile__text")
+    birth = elems2[0].getText()[:-5]
+    birthday = dt.strptime(birth,'%Y年%m月%d日').date() #文字列から日付に変換
+    birthplace = elems2[1].getText()
+    height = elems2[2].getText()[:3]
+    weight = elems2[2].getText().split('/')[1].split('kg')[0]
+    tou = elems2[3].getText()[0:2]
+    da = elems2[3].getText()[3:5]
+    touda = tou + da
+    draft = elems2[4].getText()
+    pro_year = elems2[5].getText()[:-1]
+    career = elems2[6].getText()
+
+    results = [
+        birthday,
+        birthplace,
+        height,
+        weight,
+        touda,
+        draft,
+        pro_year,
+        career
+    ]
+
+    return results
+
+if __name__=="__main__":
+    _team = get_team()
+    _number = get_player_number()
+    _name = get_player_name()
+    _position = get_player_position()
+    _results = get_player_data()
+    print("insert into player values(\'" + _team + "\', \'" + _number + "\', \'" + _name + "\', \'" + _position + "\', \'" + _results[4] + "\', " + _results[2] + ", " + _results[3]
+     + ", \'" + str(_results[0]) + "\', \'" + _results[7] + "\', " + _results[6] + ");")
