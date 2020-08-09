0018a68f2d0ae1f3c1d6c379534a535499e0343b
細田智大 <hossohdayo@hossonoMacBook.local>
2020-08-09 23:27:35 +0900

    ADD_TWITTER_CARD

--- kuri-meter/replacement_hits.sh
+++ kuri-meter/replacement_hits.sh
@@ -54,53 +54,53 @@ res1_3=`echo ${RESULT} | cut -d ',' -f 1 | awk '{print substr($0, 3, 1)}'`
 res1_4=`echo ${RESULT} | cut -d ',' -f 1 | awk '{print substr($0, 4, 1)}'`
 res2=`echo ${RESULT} | cut -d ',' -f 2`
 
-sed -ie "24s/2...年..月..日........./$res2/" $HTML_FILE
+sed -ie "31s/2...年..月..日........./$res2/" $HTML_FILE
 
 # 結果のチェック
 if [ $? -eq 0 ]; then
     echo "${TIME} [INFO] 置換OK" 2>&1 | tee -a "${LOG_FILE}"
 else
-    echo "${TIME} [ERROR] 予期せぬエラーが発生 異常終了19行目" 2>&1 | tee -a "${LOG_FILE}"
+    echo "${TIME} [ERROR] 予期せぬエラーが発生 置換1異常終了" 2>&1 | tee -a "${LOG_FILE}"
     exit 1
 fi
 
-sed -ie 26s/\>.\</\>$res1_1\</ $HTML_FILE
+sed -ie 33s/\>.\</\>$res1_1\</ $HTML_FILE
 
 # 結果のチェック
 if [ $? -eq 0 ]; then
     echo "${TIME} [INFO] 置換OK" 2>&1 | tee -a "${LOG_FILE}"
 else
-    echo "${TIME} [ERROR] 予期せぬエラーが発生 異常終了21行目" 2>&1 | tee -a "${LOG_FILE}"
+    echo "${TIME} [ERROR] 予期せぬエラーが発生 置換2異常終了" 2>&1 | tee -a "${LOG_FILE}"
     exit 1
 fi
 
-sed -ie 27s/\>.\</\>$res1_2\</ $HTML_FILE
+sed -ie 34s/\>.\</\>$res1_2\</ $HTML_FILE
 
 # 結果のチェック
 if [ $? -eq 0 ]; then
     echo "${TIME} [INFO] 置換OK" 2>&1 | tee -a "${LOG_FILE}"
 else
-    echo "${TIME} [ERROR] 予期せぬエラーが発生 異常終了22行目" 2>&1 | tee -a "${LOG_FILE}"
+    echo "${TIME} [ERROR] 予期せぬエラーが発生 置換3異常終了" 2>&1 | tee -a "${LOG_FILE}"
     exit 1
 fi
 
-sed -ie 28s/\>.\</\>$res1_3\</ $HTML_FILE
+sed -ie 35s/\>.\</\>$res1_3\</ $HTML_FILE
 
 # 結果のチェック
 if [ $? -eq 0 ]; then
     echo "${TIME} [INFO] 置換OK" 2>&1 | tee -a "${LOG_FILE}"
 else
-    echo "${TIME} [ERROR] 予期せぬエラーが発生 異常終了23行目" 2>&1 | tee -a "${LOG_FILE}"
+    echo "${TIME} [ERROR] 予期せぬエラーが発生 置換4異常終了" 2>&1 | tee -a "${LOG_FILE}"
     exit 1
 fi
 
-sed -ie 29s/\>.\</\>$res1_4\</ $HTML_FILE
+sed -ie 36s/\>.\</\>$res1_4\</ $HTML_FILE
 
 # 結果のチェック
 if [ $? -eq 0 ]; then
     echo "${TIME} [INFO] 置換OK" 2>&1 | tee -a "${LOG_FILE}"
 else
-    echo "${TIME} [ERROR] 予期せぬエラーが発生 異常終了24行目" 2>&1 | tee -a "${LOG_FILE}"
+    echo "${TIME} [ERROR] 予期せぬエラーが発生 置換5異常終了" 2>&1 | tee -a "${LOG_FILE}"
     exit 1
 fi
 
