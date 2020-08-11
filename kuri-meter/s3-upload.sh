ee2e642ed348a214a24872760463dc8ef89453ac
細田智大 <hossohdayo@hossonoMacBook.local>
2020-08-11 22:05:11 +0900

    adsense

--- kuri-meter/s3-upload.sh
+++ kuri-meter/s3-upload.sh
@@ -33,7 +33,7 @@ else
 fi
 
 # ファイルをs3にアップロード
-#/usr/local/bin/aws s3 cp ${KURI_METER_PATH}/$TARGET_FILE s3://$TARGET_BUCKET/$TARGET_FILE
+/usr/local/bin/aws s3 cp ${KURI_METER_PATH}/$TARGET_FILE s3://$TARGET_BUCKET/$TARGET_FILE
 
 # 結果のチェック
 if [ $? -eq 0 ]; then
