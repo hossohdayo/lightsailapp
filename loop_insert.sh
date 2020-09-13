06626f7bdbff164c7f672a05e4a5ca570fd9b0be
hossohdayo <hossohdayo26@gmail.com>
2020-09-13 22:07:12 +0900

    loop

---
+++ loop_insert.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+
+##############################################
+# list_player.txtを読み込んで
+# insertの場合はplayer_insert.shを実行する
+# deleteの場合は(今後別シェルを実行)
+# 引数:なし
+##############################################
+
+# 処理開始
+echo "[INFO] 処理開始"
+
+# 事前チェック
+if [ $# -ne 0 ]; then
+  echo "指定された引数は$#個です。" 1>&2
+  echo "実行するには0個の引数が必要です。" 1>&2
+  exit 1
+fi
+
+# 変数定義
+LIST_PLAYER="list_player.txt"
+ROOT_DIR=`pwd`
+LIST_PLAYER_FULL_PATH="$ROOT_DIR/$LIST_PLAYER"
+TARGET_FILE="player_insert.sh"
+TARGET_FILE_FULL_PATH="$ROOT_DIR/$TARGET_FILE"
+
+# 事前チェック
+if [ ! -e $LIST_PLAYER_FULL_PATH ]; then
+  echo "${TIME} [ERROR] LIST_PLAYERファイルが見つかりません"
+  exit 1
+fi
+
+# 事前チェック
+if [ ! -e $TARGET_FILE_FULL_PATH ]; then
+  echo "${TIME} [ERROR] TARGET_FILEファイルが見つかりません"
+  exit 1
+fi
+
+# LIST_PLAYER行数確認
+ROW=`wc -l $LIST_PLAYER_FULL_PATH | awk '{print $1}'`
+
+# 結果の終了コードチェック
+if [ $? -eq 0 ]; then
+    echo "[INFO] LIST_PLAYER行数確認OK"
+else
+    echo "[ERROR] LIST_PLAYER行数確認異常終了"
+    exit 1
+fi
+
+# LIST_PLAYER行数0はスキップ
+if [ $ROW -eq 0 ]; then
+    echo "[INFO] 0行です"
+    exit 0
+fi
+
+# メイン処理
+for i in `seq $ROW`
+do
+  # i行目を読み込む
+  LIST_PLAYER_CONTENTS=`cat $LIST_PLAYER_FULL_PATH | head -n $i | tail -n 1`
+  LIST_PLAYER_CONTENTS_1=`echo $LIST_PLAYER_CONTENTS | awk '{print $1}'`
+  LIST_PLAYER_CONTENTS_2=`echo $LIST_PLAYER_CONTENTS | awk '{print $2}'`
+  LIST_PLAYER_CONTENTS_3=`echo $LIST_PLAYER_CONTENTS | awk '{print $3}'`
+  LIST_PLAYER_CONTENTS_4=`echo $LIST_PLAYER_CONTENTS | awk '{print $4}'`
+  # insertかdeleteか判定して別シェルを叩く
+  if [ $LIST_PLAYER_CONTENTS_1 = "insert" ]; then
+    sh $TARGET_FILE_FULL_PATH $LIST_PLAYER_CONTENTS_2 $LIST_PLAYER_CONTENTS_3 $LIST_PLAYER_CONTENTS_4
+  elif [ $LIST_PLAYER_CONTENTS_1 = "delete" ]; then
+    echo "deleteシェル実行（予定）"
+  else
+    echo "[ERROR] 1文字目がdeleteまたはinsert以外"
+    exit 1
+  fi
+  # 結果の終了コードチェック
+  if [ $? -eq 0 ]; then
+      echo "[INFO] 別シェルOK"
+  else
+      echo "[ERROR] 別シェル異常終了"
+      exit 1
+  fi
+done
+
+# 結果の終了コードチェック
+if [ $? -eq 0 ]; then
+    echo "[INFO] LIST_PLAYER行数確認OK"
+    exit 0
+else
+    echo "[ERROR] LIST_PLAYER行数確認異常終了"
+    exit 1
+fi
